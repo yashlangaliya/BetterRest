8f8df9ff3c2be1ebe29fd2d626dd8e6da78211bd
yashlangaliya <yashlangaliya@gmail.com>
2020-02-06 22:38:03 +0530

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by stl-037 on 06/02/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
